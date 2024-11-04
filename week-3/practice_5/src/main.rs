a27540e389d0d3fd1187f5bb4a55f68a8d4b82f3
Nehireme Oseghael <nehioseghale@gmail.com>
2024-11-04 19:35:09 +0100

    (Week 3 practices)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@';
+    let alphabet:char = 'B';
+    let surname = "Oseghale";
+    let firstname = "Nehireme";
+    let middle_name =  "Daniella";
+
+    println!("");
+    println!("Special character {}", special_character);
+    println!("Alphabet {}", alphabet);
+    println!("Name: {} {} {}", surname, middle_name, firstname);
+}
