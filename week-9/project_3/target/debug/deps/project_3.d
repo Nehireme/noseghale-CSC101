f09d0a94598243c8eef1d124e04214c9b5ac004d
Nehireme Oseghael <nehioseghale@gmail.com>
2025-01-04 23:46:33 +0100

    (week 9)

---
+++ week-9/project_3/target/debug/deps/project_3.d
@@ -0,0 +1,5 @@
+C:\Users\hp\OneDrive\Documents\noseghale-CSC101\week-9\project_3\target\debug\deps\project_3.exe: src/main.rs
+
+C:\Users\hp\OneDrive\Documents\noseghale-CSC101\week-9\project_3\target\debug\deps\project_3.d: src/main.rs
+
+src/main.rs:
